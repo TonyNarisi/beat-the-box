1202268f8d956be156b88a438bf61f5e880b2ba1
Tony Narisi <tonynarisi@gmail.com>
2016-10-18 20:27:45 -0400

    remove debugging code

--- app/assets/javascripts/components/play-game.es6.jsx
+++ app/assets/javascripts/components/play-game.es6.jsx
@@ -8,22 +8,13 @@ class PlayGame extends React.Component {
   }
 
   componentDidMount() {
-    // for (var i = 0; i < 9; i++) {
-      // card = this.props.deck[i];
-      // this.setState({
-      //   box: this.state.box.concat([card])
-      // })
-      // debugger;
-    // }
     this.setState({
       box: this.props.deck.slice(0, 9),
       remainingDeck: this.props.deck.slice(9, 53)
     })
-    debugger;
   }
 
   render(){
-    debugger;
     return (
       <div>
         <p>Welcome to the game playing screen!</p>
